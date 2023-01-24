0f440bf04f59468d7dc8e4573bbf7551780c218f
Pukhraj Choudhary <120653585+Pukhraj85@users.noreply.github.com>
2023-01-24 20:16:59 +0530

    Update Footer.jsx

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
 
         <p>Contact me : </p>
         <a href={"https://www.linkedin.com/in/pukhraj-choudhary-2b31a4247/"} className="btn btn-ghost btn-sm rounded-btn">Linkedin</a>
-        <a href={process.env.REACT_APP_GITHUB_URL_PERSONAL} className="btn btn-ghost btn-sm rounded-btn">Github</a>
+        <a href={https://github.com/Pukhraj85} className="btn btn-ghost btn-sm rounded-btn">Github</a>
       </div>
     </footer>
   );
